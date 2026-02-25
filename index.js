75ee06078f99701ed651b9f1546025e65da62845
DennisAlwyn <104767447+DennisAlwyn@users.noreply.github.com>
2026-02-25 16:48:28 +0800

    Fixes and Cleanup
    - Reorganized index.js
    - Fixed the header animation on rotate(for real, hopefully)
    - Fixed link text alignment

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 //Check if user on mobile
 const isMobile = window.matchMedia("(any-pointer: coarse)").matches;
 
+//Called at page load, could add awake() to mimic Unity later idk...
+function start(){
+    initEmailCopy();
+    stopHeaderAnim ();
+}
+
+//Blocks the header animation unless page refresh
+function stopHeaderAnim(){
+    let headerElem = document.getElementById("headerElem");
+    let rootStyles =  window.getComputedStyle(document.documentElement);
+    //Get Delay from CSS in ms
+    let totalDelay = parseFloat(rootStyles.getPropertyValue('--startanimdelay') + rootStyles.getPropertyValue('--startanimduration')) * 10000;
+    console.log(totalDelay);
+    
+    //Add no anim after finishing animation
+    setTimeout(() => {
+        headerElem.classList.add("no-animation");
+
+        headerElem = null;
+        rootStyles = null;
+        totalDelay = null;
+    }, totalDelay);
+}
+
 function initEmailCopy(){
     //Get all emails
     let emails = document.getElementsByClassName("email");
@@ -32,4 +56,4 @@ function initEmailCopy(){
     emails = null;
 }
 
-initEmailCopy();
+start()
